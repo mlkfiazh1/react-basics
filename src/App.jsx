38049a1ed4f87d333e9d25a5cfa02bc043b79cbc
fiaz <mlkfiazh1@gmail.com>
2026-07-08 20:49:17 +0500

    add pizza component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const Pizza = (props) => {
-  // return React.createElement('div', {}, [
-  //   React.createElement('h1', {}, props.name),
-  //   React.createElement('p', {}, props.description),
-  // ]);
-
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <p>{props.description}</p>
-    </div>
-  );
-};
+import Pizza from './Pizza';
 
 const App = () => {
-  // return React.createElement('div', {}, [
-  //   React.createElement('h1', {}, 'Pixel Perfect Pizzas 1'),
-  //   React.createElement(Pizza, {
-  //     name: 'The Pepperoni Pizza',
-  //     description: 'Mozzarella Cheese, Pepperoni',
-  //   }),
-  //   React.createElement(Pizza, {
-  //     name: 'The Hawaiian Pizza',
-  //     description: 'Sliced Ham, Pineapple, Mozzarella Cheese',
-  //   }),
-  // ]);
-
   return (
     <div>
-      <h1>Pixel Perfect Pizzas 2</h1>
+      <h1>Pixel Perfect Pizzas</h1>
       <Pizza name="The Pepperoni Pizza" description="Mozzarella Cheese, Pepperoni" />
       <Pizza name="The Hawaiian Pizza" description="Sliced Ham, Pineapple, Mozzarella Cheese" />
     </div>
